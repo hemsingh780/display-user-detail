8c75a73b4728010959c7e3393edb0c0613a8f162
hem <hemsinghdhoni_bca18_21@its.edu.in>
2021-06-14 12:05:41 +0530

    display user detail file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import Pagination from '@material-ui/lab/Pagination';
+import Page2 from './Page2/Page2';
+import Page1 from './Page1/Page1';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+ function App() {
+
+  const [page,setPage] = useState({
+    rederData:true,
+  })
+
+  const change = (e) =>{
+    const data = page.rederData
+    setPage({rederData:!data})
+  }
+  
+  let Page = null;
+  if(page.rederData){
+    Page = <Page1 />
+  }else{
+    Page = <Page2 />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+    <div  className='pagination'>
+    
+    <Pagination 
+    color='secondary'
+    count={2}
+    onChange={change}
+    />
+
+    </div>
+    {Page}
     </div>
   );
 }
---
+++ src/Page1/Page1.js
@@ -0,0 +1,59 @@
+
+import { useEffect, useState } from 'react'
+import classes from './Page1.module.css'
+import '../Loader.css'
+
+const Page1 = () =>{
+
+    const [state,setState] = useState({
+        loading:true,
+        user:[]
+    })
+
+    useEffect(async ()=>{
+    const response = await fetch('https://reqres.in/api/users?page=1')
+    const data = await response.json();
+    let userData = []
+    for(const key in data.data){
+         userData.push(
+            data.data[key]
+         )
+    }
+     setState({user:userData,loading:false})
+    },[])
+
+    console.log(state.user)
+    const style = {
+        width:'250px',
+        height:'230px'
+    }
+    const page2UserList = state.user.map((x,i) => {
+        return(
+            <div className={classes.card} key={x.id}>
+                
+                <img style={style} src={x.avatar} alt='avatar'/>
+                <div className={classes.Detail}>
+                <p>First Name:{ x.first_name}</p>
+                <p>Last Name:{ x.last_name}</p>
+                <p>Email:{ x.email}</p>
+                </div>
+            </div>
+        )
+    })
+
+    let data = null
+    if(state.loading || !state.user){
+        data = <div className="loader">Loading...</div>
+    }else{
+        data =<div className={classes.card2}>{page2UserList}</div>
+    }
+
+    return(
+         <>
+          {data}
+         </>
+     )
+
+}
+
+export default Page1
